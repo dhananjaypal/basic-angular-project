ff7f0162f0ae3e6a431a60a6bd872b463457d485
Dhananjay Pal <106646125+dhananjaypal@users.noreply.github.com>
2023-12-03 10:58:01 +0000

    Added components and routing

---
+++ basic-angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+
+// Define your routes
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
