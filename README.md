39940c48c9d01b52f2c9072d2e7f43b2030344e6
Dhananjay Pal <106646125+dhananjaypal@users.noreply.github.com>
2023-12-03 05:57:28 +0000

    Added navbar component

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# basic-angular-project
+# Creating a new module--
+  type command ng generate module myNewModule
+  add MyNewModuleModule in imports array of app.component.ts file
+
+# Creating new component in newly created myNewModule
+   type command ng g c my-new-component
+   add MyNewComponentComponent in imports array of app.component.ts file
+   add <app-my-new-component></app-my-new-component> in any html file where you want to include this new component.
+   For Exp: app.component.html
+
+
--- basic-angular-app/src/app/app.component.ts
+++ basic-angular-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MyNewModuleModule } from './my-new-module/my-new-module.module';
 import { MyNewComponentComponent } from './my-new-module/my-new-component/my-new-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MyNewModuleModule, MyNewComponentComponent],
+  imports: [CommonModule, RouterOutlet, MyNewModuleModule, MyNewComponentComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
