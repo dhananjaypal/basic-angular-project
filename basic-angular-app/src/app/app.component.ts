de95b1cc3211b4f6bb64cc0a5f986d9f5cd39801
Dhananjay Pal <106646125+dhananjaypal@users.noreply.github.com>
2023-12-03 04:49:21 +0000

    Added a new component in myNewModule

--- basic-angular-app/src/app/app.component.ts
+++ basic-angular-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MyNewModuleModule } from './my-new-module/my-new-module.module';
+import { MyNewComponentComponent } from './my-new-module/my-new-component/my-new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MyNewModuleModule],
+  imports: [CommonModule, RouterOutlet, MyNewModuleModule, MyNewComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
